1275ad4b3f9b3e0062839f93110b08b457c29e62
Sarthak Agarwal <agarwalsarthak121@gmail.com>
2021-02-10 18:48:30 +0530

    Added hashmap and parking lot design starter code

---
+++ HashMap/src/main/java/com/sarthak/customHashMap/MyMap.java
@@ -0,0 +1,8 @@
+package com.sarthak.customHashMap;
+
+public class MyMap<K,V> {
+	
+	Node<K,V>[] buckets;
+	public static final int INITIAL_CAPACITY = 16; 
+
+}
---
+++ HashMap/src/main/java/com/sarthak/customHashMap/Node.java
@@ -0,0 +1,8 @@
+package com.sarthak.customHashMap;
+
+public class Node<K,V> {
+	K key;
+	V value;
+	int hash;
+	Node<K,V> next;
+}
